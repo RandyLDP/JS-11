67e712ddf59a6d313e7266fc51da0d1560b95db6
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-06-01 19:41:21 +0200

    Update multiplication-table.js

--- multiplication-table.js
+++ multiplication-table.js
@@ -1,4 +1,5 @@
-let num = 9;
-for (i = 1; i<=10; i++){
-    console.log(i +  "x" + num + "=" + (i * num));
+const multiplier = 9;
+for (let i = 0; i <= 10; i++) {
+  let result = multiplier * i;
+  console.log(multiplier + " * " + i + " = " + result);
 }
